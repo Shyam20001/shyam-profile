bfa5d3e72888ce14f46fcbb1eb4185fd3fd0554c
Shyam <mshayam41@gmail.com>
2024-10-16 09:44:25 +0530

    TEST

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'shyam-profile',
+      short_name: 'shyam-profile',
+      description: 'This is PWA portfolio created by Shyam. M (A passionate NODE JS dev)',
+      theme_color: '#00b300',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+  base: "/shyam-profile/"
+})
